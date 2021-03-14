e761500c4de7946611d6d64290b09d14d9ea2492
finnley <finnley.yuan@jingdigital.com>
2021-03-14 12:32:18 +0800

    health check

--- initialize/router.go
+++ initialize/router.go
@@ -4,11 +4,20 @@ import (
 	"cat-user-api/middlewares"
 	"cat-user-api/routers"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Routers() *gin.Engine {
 	Router := gin.Default()
-	//配置跨域
+	// 用来给consul做健康检查使用
+	Router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"success": true,
+		})
+	})
+
+	// 配置跨域
 	Router.Use(middlewares.Cors())
 	ApiGroup := Router.Group("/u/v1")
 	routers.InitBaseRouter(ApiGroup)
